20e92ebf0fa9382661654e6172dadf568ab66b76
yuweihan <yuweihan@mail.dlut.edu.cn>
2016-11-11 10:04:10 +0800

    something of texs

---
+++ jqurey.color.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/11/11 0011.
+ */
+;(function($){
+    $.fn.extend({
+        'color':function(value){
+            return this.css('color',value)
+        }
+    });
+})(jQuery);
